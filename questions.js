9dac079f6e470c8515db9f2da9108a652a955644
Suuto <65576559+Suuto@users.noreply.github.com>
2020-05-19 07:52:47 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,103 @@
+const questions = [
+  {
+    "question": "How many people will live in your house?",
+    "answer1": "4 or less",
+    "answer1Total": "1",
+    "answer2": "5-6",
+    "answer2Total": "2",
+    "answer3": "over 30",
+    "answer3Total": "3"
+  },
+  {
+    "question": "You can only have one of these in your house",
+    "answer1": "Swimming Pool",
+    "answer1Total": "1",
+    "answer2": "Big Empty Space Outside",
+    "answer2Total": "2",
+    "answer3": "Garden",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Which type of neighbor do you hate the most?",
+    "answer1": "Loud neighbor",
+    "answer1Total": "1",
+    "answer2": "Selfish neighbor",
+    "answer2Total": "3",
+    "answer3": "Anti-social neighbor",
+    "answer3Total": "2"
+  },
+  {
+    "question": "Which kind of car do you like?",
+    "answer1": "Sport car",
+    "answer1Total": "3",
+    "answer2": "Luxury Car",
+    "answer2Total": "2",
+    "answer3":
+      "Van",
+    "answer3Total": "1"
+  },
+  {
+    "question": "Which best describes your relationship with food",
+    "answer1": "Bedroom",
+    "answer1Total": "1",
+    "answer2": "Living Room",
+    "answer2Total": "2",
+    "answer3": "Kitchen",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "Which kind of animal do you want to have?",
+    "answer1":
+      "Cat",
+    "answer1Total": "3",
+    "answer2": "Dog",
+    "answer2Total": "2",
+    "answer3": "Bird",
+    "answer3Total": "1"
+  },
+  {
+    "question":
+    "Choose the colors for your house?",
+    "answer1": "Grey Black White",
+    "answer1Total": "1",
+    "answer2": "Green White Brown",
+    "answer2Total": "2",
+    "answer3": "Yellow Gold White",
+    "answer3Total": "3"
+  },
+  {
+    "question":
+      "What is your hobby?",
+    "answer1":
+      "Sports",
+    "answer1Total": "3",
+    "answer2": "Listening to music",
+    "answer2Total": "2",
+    "answer3": "Reading",
+    "answer3Total": "1"
+  },
+  {
+    "question":
+      "Choose your breakfast!",
+    "answer1":
+      "American Breakfast (Eggs + Sausage + Bacon +Bread)",
+    "answer1Total": "3",
+    "answer2": "Breakfast Sandwich set",
+    "answer2Total": "2",
+    "answer3": "McDonald Breakfast (Mc Chicken + Rice + Egg + Coffee)",
+    "answer3Total": "1"
+  },
+  {
+    "question":
+      "Which of these is your biggest wish?",
+    "answer1":
+      "10 million dollars",
+    "answer1Total": "3",
+    "answer2": "Free food for the rest of your life",
+    "answer2Total": "2",
+    "answer3": "Traveling around the world for free",
+    "answer3Total": "1"
+  }
+]
